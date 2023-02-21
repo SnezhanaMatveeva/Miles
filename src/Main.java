c43f4905662b670ec6715fc3a7604ac0c052ec6a
SnezhanaMatveeva <109746701+SnezhanaMatveeva@users.noreply.github.com>
2023-02-21 06:37:22 +0300

    Update Main.java
    change name of int quantityRublesForOneBonusMile to int rublesBonusMile

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int ticketPrice = 5731;
-        int quantityRublesForOneBonusMile = 20;
+        int rublesBonusMile = 20;
         int mile = ticketPrice / quantityRublesForOneBonusMile;
         System.out.println(mile);
     }
