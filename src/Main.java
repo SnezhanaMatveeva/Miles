ad7b506ebe970cca858b52e4a9cf2d4f0f4afc77
SnezhanaMatveeva <109746701+SnezhanaMatveeva@users.noreply.github.com>
2023-02-21 06:39:26 +0300

    Update Main.java
    change int rublesBonusMile

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 5731;
         int rublesBonusMile = 20;
-        int mile = ticketPrice / quantityRublesForOneBonusMile;
+        int mile = ticketPrice / rublesBonusMile;
         System.out.println(mile);
     }
 
