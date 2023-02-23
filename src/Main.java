6f6959cf8af681f81465a5a2dcef0c9929d0d09b
SnezhanaMatveeva <12345fam@mail.ru>
2023-02-23 22:44:04 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 5731;
-        int rublesBonusMile = 20;
-        int mile = ticketPrice / rublesBonusMile;
-        System.out.println(mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
-
 }
+
+
+
